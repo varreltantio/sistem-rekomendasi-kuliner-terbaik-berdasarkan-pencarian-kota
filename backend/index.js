799615a626cda1123b3f06ca2640c45b9141a74f
varreltantio <snetid90@gmail.com>
2023-07-09 00:27:47 +0700

    change config express

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,19 @@ import edgeChromium from 'chrome-aws-lambda'
 import puppeteer from 'puppeteer-core'
 
 const app = express();
-app.use(cors({ origin: '*' }));
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 const LOCAL_CHROME_EXECUTABLE = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 
